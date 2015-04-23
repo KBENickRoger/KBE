c4c76688ace7d28096734bb33cb7d8ae6aeff671
rjcoenen <rjcoenen@hotmail.com>
2015-04-23 16:31:55 +0200

    new fuselage

--- Code/source/Fuselage.lisp
+++ Code/source/Fuselage.lisp
@@ -1,17 +1,28 @@
-(in-package :kbe)
+(in-package :gdl-user)
 
-(define-object Fuselage ()
+(define-object Fuselage (geom-base:cylinder)
   
   :documentation
   (:author "<name> (<username>@<organization>.com)"
    :description "")
   
   :input-slots
-  ()
+  ((""
+    fuselageLengthTotal)
+   
+   (""
+    fuselageTailRotAngle nil #|todo: default value|#)
+   
+   (""
+    fuselageSlenderness))
   
   
   :computed-slots
-  ()
+  ((""
+    radius (/ (the fuselageLengthTotal) (the fuselageSlenderness) 2) )
+   
+   (""
+    length (the fuselageLengthTotal)))
   
   
   :objects
@@ -20,4 +31,6 @@
   
   :functions
   ())
+
+
 
