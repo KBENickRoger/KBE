c59dcaf6235d6fe5e32c1019962d4c88387ff87e
rjcoenen <rjcoenen@hotmail.com>
2015-04-24 10:51:43 +0200

    aerocap

---
+++ Code/source/Aerodynamics(cap).lisp
@@ -0,0 +1,54 @@
+(in-package :gdl-user)
+
+(define-object Aerodynamics(cap) ()
+  
+  :documentation
+  (:author "<name> (<username>@<organization>.com)"
+   :description "Aero")
+  
+  :input-slots
+  ()
+  
+  
+  :computed-slots
+  ((""
+    Beta nil #|todo|# )
+   
+   (""
+    CLalpha nil #|todo|# )
+   
+   (""
+    CLalphaWF nil #|todo|# )
+   
+   (""
+    Vh_V nil #|todo|# )
+   
+   (""
+    dEpsdAlph nil #|todo|# )
+   
+   (""
+    Keps nil #|todo|# )
+   
+   (""
+    Keps0 nil #|todo|# )
+   
+   (""
+    wingSweep05 nil #|todo|# )
+   
+   (""
+    wingAreaNet nil #|todo|# )
+   
+   (""
+    wingArea nil #|todo|# )
+   
+   (""
+    wingSpanNet nil #|todo|# ))
+  
+  
+  :objects
+  ()
+  
+  
+  :functions
+  ())
+
