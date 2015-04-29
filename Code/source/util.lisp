d671c0a41f5c6eb8246b5b2822b570597ab2a895
Madroon <nmnoordam@gmail.com>
2015-04-29 14:36:39 +0200

    Update util.lisp

--- Code/source/util.lisp
+++ Code/source/util.lisp
@@ -2,6 +2,19 @@
 ;;; This file contains utility functions used in the entire package.
 ;;;
 
-(in-package :kbe)
+(in-package :gdl-user)
 
-;; TODO: add your utility functions here
+
+;; Input data reading functions
+
+;; read string data - input file location - output list of strings read line by line from file
+(defun readStringData (fileName)
+  (with-open-file (in fileName)
+    (let (result)
+      (do ((line (read-line in nil nil) (read-line in nil nil)))
+	  ((or (null line)(string-equal line ""))(nreverse result))
+	(push line result))
+)))
+
+(defun splitData (dataList)
+ (mapcan 'split dataList))
