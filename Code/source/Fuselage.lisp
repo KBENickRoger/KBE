db5813d17c617027f59362a193aa9b16497f6ba3
Madroon <nmnoordam@gmail.com>
2015-05-02 16:56:10 +0200

    fuselage.lisp update
    fuselage bestaat nu uit drie secties

--- Code/source/Fuselage.lisp
+++ Code/source/Fuselage.lisp
@@ -1,26 +1,41 @@
 (in-package :gdl-user)
 
-(define-object Fuselage ()
+(define-object Fuselage (base-object)
   
   :documentation
-  (:author "<name> (<username>@<organization>.com)"
+  (:author ""
    :description "")
   
   :input-slots
   ((""
-    length 10)
+    lengthTotal 10)
    
    (""
-    slenderness 2)
+    finenessRatio 2)
+	
+   (""
+	lengthCenter 5)
+	
+	(""
+	lengthNose 2)
   )
   
-  
   :computed-slots
-  ()
+  ((diameter (/ (the lengthTotal) (the finenessRatio)))
+   (lengthTail (- (the lengthTotal) (the lengthCenter)))
+  
+  )
   
 
   :objects 
-  ()
+  ((fuselageCenter 	:type 'cylinder
+					:length (the lengthCenter)
+					:radius (half (the diameter))
+					:center (translate (the center)
+						:rear (- (+ (the lengthNose) (half (the lengthCenter)))))
+	)
+
+  )
 
   :functions
   ())
