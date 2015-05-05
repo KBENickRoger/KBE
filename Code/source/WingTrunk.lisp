1d58b3e048537f0d0a338d1472e50d19fa2cd995
Madroon <nmnoordam@gmail.com>
2015-05-05 15:47:54 +0200

    forgot a fil

--- Code/source/WingTrunk.lisp
+++ Code/source/WingTrunk.lisp
@@ -26,17 +26,83 @@
   :computed-slots
   ((length (the chordRoot))
 	(width (the span))
-	(height 1)
+	(height (the thickness))
 	(airfoilFile (merge-pathnames (the airfoil) (the dataFolder)))
 	(pointsData (with-open-file (in (the airfoilFile)) (read in)))
+	(thickness 1)
 	)
   
   
   :objects
   ((box
-	:type 'box))
-  
+	:type 'box)
+	
+	(profile 
+	:type 'profile-curve
+	:points-data (the pointsData)
+	:hidden? t
+	)
+	
+	(root-profile :type 'boxed-curve
+	:curve-in (the profile)
+	:orientation (alignment :top (the (face-normal-vector :right))
+			:rear (the (face-normal-vector :top))
+			:right (the (face-normal-vector :rear)))
+	:scale-y (/ (the thickness) (the profile max-thickness))
+	:scale-x (/ (the chordRoot) (the profile chord))
+	:center (the (edge-center :left :front))
+	:hidden? t
+)
+
+	(tip-profile :type 'boxed-curve
+	:curve-in (the profile)
+	:orientation (alignment :top (the (face-normal-vector :right))
+			:rear (the (face-normal-vector :top))
+			:right (the (face-normal-vector :rear)))
+	:scale-y (/ (the thickness) (the profile max-thickness))
+	:scale-x (/ (the chordTip) (the profile chord))
+	:center (translate (the (edge-center :right :front))
+			:rear (- (the length) (the chordTip)))
+	:hidden? t
+	)
+	
+	(loft :type 'lofted-surface
+	:end-caps-on-brep? t
+	:curves (list (the root-profile) (the tip-profile)))
+	:hidden t
+	)  
   
   :functions
   ())
+
+  
+  
+(define-object profile-curve (fitted-curve)
+  :input-slots (points-data)
+  
+  :computed-slots 
+  (
+  
+  (data-name (string-append (first (the points-data))
+							(second (the points-data))))
+
+	(point-coordinates (rest (rest (the points-data))))
+
+(x-coords (plist-keys (the point-coordinates)))
+(y-coords (plist-values (the point-coordinates)))
+
+(max-x (most 'get-x (the points)))
+(min-x (least 'get-x (the points)))
+(max-y (most 'get-y (the points)))
+(min-y (least 'get-y (the points)))
+
+(chord (- (get-x (the max-x))
+(get-x (the min-x))))
+
+(max-thickness (- (get-y (the max-y))
+(get-y (the min-y))))
+
+(points (mapcar #'(lambda(x y) (make-point x y 0))
+(the x-coords)
+(the y-coords)))))
 
