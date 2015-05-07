242344fbc39c613bc03375dea80aa957f64c23bf
rjcoenen <rjcoenen@hotmail.com>
2015-05-07 16:15:43 +0200

    Revert "joe"
    This reverts commit 8c8a505035f28ed11e36da440ccb14373a669a61.

--- Code/source/ConTail.lisp
+++ Code/source/ConTail.lisp
@@ -7,52 +7,16 @@
    :description "")
   
   :input-slots
-  ((tailTaperHor 0.3 :settable)
-    
-  (tailSweepV 35 :settable
-  ))
+  ((TaperHor :settable)
+  ()
+  )
   
   
   :computed-slots
-  (("Surface areas horizontal tail"
-  tailSurfaceAreaHor (/ (* (the tailHorVolume) (the wingSurfaceArea) (the wingRootChord)) (the tailArmH)))
+  ((SurfaceAreaHor ())
+  (SurfaceAreaVert))
   
-  ("Surface areas vertical tail"
-  tailSurfaceAreaVer (/ (* (the tailVerVolume) (the wingSurfaceArea) (the wingSpan)) (the tailArmV)))
   
-  ("Sweep of the horizontal tail"
-  tailSweepHor (+ (the wingSweep) 10))
-  
-  ("Taper ratio of vertical tail"
-  case tailType (:1 (tailTaperVer 0.3)) (:2 (tailTaperVer 0.7)) (:3 (tailTaperVer 0.7)))
-  
-  ("Aspect ratio of vertical tail"
-  case tailType (:1 (tailARVer 1.9)) (:2 (tailARVer 1.2 :settable)) (:3 (tailARVer 1.2 :settable)))
-  
-  ("Horizontal tail span"
-	tailSpanHor (sqrt(* (the tailARhor) (the tailSurfaceAreaHor))))
-  
-  ("Vertical tail span"
-  tailSpanVer (sqrt(* (the tailARVer) (the tailSurfaceAreaVer))))
-  
-  ("Rootchord Vertical tail"
-  tailRootChordVer (/ (* 2 (the tailSurfaceAreaVer)) (* (the tailSpanVer) (+ 1 (the tailTaperVer))))
-  )
-  
-  ("Tip chord Vertical tail"
-  tailTipChordVer (* (the tailTaperVer) (the tailTaperVer))
-  )
-  
-  ("Root chord Horizontal tail"
-  tailRootChordHor (/ (* 2 (the tailSurfaceAreaHor)) (* (the tailSpanHor) (+ 1 (the tailTaperHor))))
-  )
-  
-  ("Tip chord Horizontal tail"
-  tailTipChordHor (* (the tailTaperHor) (the tailTaperHor))
-  )
-  
- ("vertical placing of horizontal tail")
-  case (tailType (:1 ) (:2 ) (:3 )))
   :objects
   ((""
     wingTrunk :type 'WingTrunk))
