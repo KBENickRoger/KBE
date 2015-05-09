0612755028938d4b728aaa94de5ee0e16e2d16c1
rjcoenen <rjcoenen@hotmail.com>
2015-05-09 15:27:08 +0200

    todays effort on sizing
    Have made a selectionmethod. only if all 3 positions match, the volume
    gets added to a list and then averages it

--- Code/source/TailSizing.lisp
+++ Code/source/TailSizing.lisp
@@ -15,20 +15,28 @@
   
   
   :computed-slots
+  ;Read in file with aircraft
   ((inputDataFilePath1 (merge-pathnames (the inputDataTailfilename) (the dataFolder)))
   (inputDataFilePath2 (merge-pathnames (the inputDataTailVolHorfilename) (the dataFolder)))
-  (inputDataFilePath3 (merge-pathnames (the inputDataTailVolVerfilename) (the dataFolder)))
+  (inputDataFilePath3 (merge-pathnames (the inputDataTailVolVerfilename) (the dataFolder))
+  )
   
   (inputDataTail (basicDataReader (the inputDataFilePath1)))
   (inputDataTailVolHor (basicDataReader (the inputDataFilePath2)))
   (inputDataTailVolVer (basicDataReader (the inputDataFilePath3)))
   
+  ;!!!!! A function to add the variable names in the plist with every a/c is still too be added.!!!!!!!!!!!!!!!!!!!
   
+  ;Selection function to use only appropriate aircraft
+  (mapcar #'(if (and (:num = (the EngineNum)) (:pos = (the EnginePos)) (:tail = (the tailType))) (append :volume 'VolumelistH)) 'InputdataTailVolHor)
+  
+  ; Average tailvolume coefficient
   (("Statistically based Horizontal Tail volume Coefficient"
-    TailVolHor nil #|todo|# )
+    tailVolHor (/ (sum-elements (the VolumelistH)) (:length (the VolumelistH)) )
    
    ("Statistically based Vertical Tail volume Coefficient"
-    TailVolVer nil #|todo|# )))
+    tailVolVer (/ (sum-elements (the VolumelistV)) (:length (the VolumelistV)) )
+	)
   
   
   :objects
