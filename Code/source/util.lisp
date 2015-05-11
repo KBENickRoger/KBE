c4b265579b93b0e1f35a9d921f962f504d18e657
rjcoenen <rjcoenen@hotmail.com>
2015-05-11 21:56:38 +0200

    hope it'll work, ill try tomorrow!

--- Code/source/util.lisp
+++ Code/source/util.lisp
@@ -46,3 +46,21 @@
 (defun basicNumberReader (filename)
 	(mapcar 'parser (mapcan 'split (readStringData filename)))
 )
+
+;; Add headers to tailvolume data
+(defun make-headers (strings &aux (package "KEYWORD"))
+               (mapcar (lambda (name)
+                         (intern (string-upcase name) package))
+                       strings))
+;; MAKE-HEADERS
+
+(defparameter *headers* (make-headers '("name" "num" "pos" "tail" "volume"))) 
+
+;; Pair entry with header
+(defun read-csv-line (&key (headers *headers*) line)
+               (pairlis headers line))
+			   
+;; Same function as Basicdatareader only with predefined header and 5 data columns
+(defun advancedDataReader (filename)
+		(mapcar 'read-csv-line (&key (headers *headers*) (splitData dataList))
+				)
