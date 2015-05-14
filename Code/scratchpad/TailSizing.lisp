bd76df495a15ae0480d5c469f6ba594d2feb6e63
Madroon <nmnoordam@gmail.com>
2015-05-14 21:19:58 +0200

    input selection mechanism

--- Code/scratchpad/TailSizing.lisp
+++ Code/scratchpad/TailSizing.lisp
@@ -14,26 +14,46 @@
   
   :computed-slots
   ( 
+  
+  (selection (select (where :engineNumber (the input engineNumber) :engineMounting (the input engineMounting) :tailType (the input tailType))))
+  
+  
   ;Selection function to use only appropriate aircraft
   ;Still needs accounting for C, H or Vtail (no matching tails)
-  (tailVolHor (mapcan #'(if (and (:num = (the EngineNum)) (:pos = (the EnginePos)) (:tail = (the tailType))) (append :volume 'VolumelistH)) 'InputdataTailVolHor)
+  (tailVolHor (mapcan #'(if (and (:num = (the EngineNum)) (:pos = (the EnginePos)) (:tail = (the tailType))) (append :volume 'VolumelistH)) 'InputdataTailVolHor))
   
   ; Average tailvolume coefficient
-  (("Statistically based Horizontal Tail volume Coefficient"
-    tailVolHor (/ (sum-elements (the VolumelistH)) (:length (the VolumelistH)) )
+  ("Statistically based Horizontal Tail volume Coefficient"
+    tailVolHor (/ (sum-elements (the VolumelistH)) (:length (the VolumelistH)) ))
    
    ("Statistically based Vertical Tail volume Coefficient"
-    tailVolVer (/ (sum-elements (the VolumelistV)) (:length (the VolumelistV)) )
-	)
-  
+    tailVolVer (/ (sum-elements (the VolumelistV)) (:length (the VolumelistV)) ))
+
+  )
   
   :objects
   ()
   
   
   :functions
-  ( (selector ())
+  ( )
   
   
-  ))
+  )
 
+  (defun select (selector-fn db)
+	(remove-if-not selector-fn db))
+
+  (defun where (&key engineNumber engineMounting tailType)
+	#'(lambda (entry)
+		(and
+		(if engineNumber (= (getf entry :engineNumber) engineNumber) t)
+		(if engineMounting (= (getf entry :engineMounting) engineMounting))
+		(if tailType (= (getf entry :tailType) tailType))))
+	)
+	
+	(defun sumVolume (list)
+		(if (null list) 0 
+			(+ (getf :tailVolumeHorizontal (first list)) (sumVolume (rest list)))
+		)
+	)
