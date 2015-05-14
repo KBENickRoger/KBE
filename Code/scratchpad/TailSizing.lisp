f46341b20aa10645049bbf4d6e192ee0b1e22160
Madroon <nmnoordam@gmail.com>
2015-05-14 17:22:50 +0200

    cleaned up tailSizing

--- Code/scratchpad/TailSizing.lisp
+++ Code/scratchpad/TailSizing.lisp
@@ -7,27 +7,16 @@
    :description "File to size the tail from statistical data input, by means of selecting appropriate aircraft and averaging their tail volume coefficients")
   
   :input-slots
-   ((dataFolder *dataFolder*)
-   (inputDataTailfilename "inputDataTail.dat")
-   (inputDataTailVolHorfilename "tailDataHor.dat")
-   (inputDataTailVolVerfilename "tailDataVer.dat")
+  ((input)
+   (database)
   )
   
   
   :computed-slots
-  ;Read in file with aircraft
-  ((inputDataFilePath1 (merge-pathnames (the inputDataTailfilename) (the dataFolder)))
-  (inputDataFilePath2 (merge-pathnames (the inputDataTailVolHorfilename) (the dataFolder)))
-  (inputDataFilePath3 (merge-pathnames (the inputDataTailVolVerfilename) (the dataFolder))
-  )
-  
-  (inputDataTail (advancedDataReader (the inputDataFilePath1)))
-  (inputDataTailVolHor (advancedDataReader (the inputDataFilePath2)))
-  (inputDataTailVolVer (advancedDataReader (the inputDataFilePath3)))
-    
+  ( 
   ;Selection function to use only appropriate aircraft
   ;Still needs accounting for C, H or Vtail (no matching tails)
-  (mapcar #'(if (and (:num = (the EngineNum)) (:pos = (the EnginePos)) (:tail = (the tailType))) (append :volume 'VolumelistH)) 'InputdataTailVolHor)
+  (tailVolHor (mapcan #'(if (and (:num = (the EngineNum)) (:pos = (the EnginePos)) (:tail = (the tailType))) (append :volume 'VolumelistH)) 'InputdataTailVolHor)
   
   ; Average tailvolume coefficient
   (("Statistically based Horizontal Tail volume Coefficient"
@@ -43,5 +32,8 @@
   
   
   :functions
-  ())
+  ( (selector ())
+  
+  
+  ))
 
