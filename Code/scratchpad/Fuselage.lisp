9583882e1ca82a8d507ca901cd777c583142ff3c
rjcoenen <rjcoenen@hotmail.com>
2015-05-19 17:01:10 +0200

    oke dit is dan de echte

--- Code/scratchpad/Fuselage.lisp
+++ Code/scratchpad/Fuselage.lisp
@@ -2,9 +2,9 @@
 
 (define-object fuselagetail (base-object)
 :input-slots
-((diameter 2)
-(divergence 15)
-(lengthTail 15)
+((diameter 2 :settable)
+(divergence 15 :settable)
+(lengthTail 15 :settable)
 (cross-section-percentages (list 0 0.5 0.9))
 (tailPoint (make-point 0 0 0))
 (lengthCenter 25)
@@ -20,22 +20,24 @@
 (divergenceRAD (DEGREES-TO-RADIANS (the divergence)))
 )
 
-;:objects 
-((section-curves :type 'arc-curve
+
+
+:objects 
+((merged :type 'merged-solid
+:brep (the loft brep)
+:other-brep ()
+:make-manifold? t)
+
+(section-curves :type 'arc-curve
 :sequence (:size (length (the section-offset-length)))
 :center (translate (the tailPoint)
 					:rear (+ (the lengthNose) (the lengthCenter) (nth (the-child index) (the section-offset-length)))
-			;		:down (- (first (the section-radii)) (nth (the-child index) (the section-radii)))
+					:down (- (first (the section-radii)) (nth (the-child index) (the section-radii)))
 					:up (nth (the-child index) (the section-offset-divergence)))
 :radius (nth (the-child index) (the section-radii))
 :orientation (alignment :top (the (face-normal-vector :front))))
-)
-)
-;(merged :type 'merged-solid
-;:brep (the loft brep)
-;:make-manifold? t)
 
-;(loft :type 'lofted-surface
-;:end-caps-on-brep? t
-;:curves (list-elements (the section-curves)))
-;))
+(loft :type 'lofted-surface
+:end-caps-on-brep? t
+:curves (list-elements (the section-curves))))
+)
