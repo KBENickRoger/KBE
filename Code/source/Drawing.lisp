6c2986d6733965dc6d90d79cb572622cffea884e
rjcoenen <rjcoenen@hotmail.com>
2015-05-23 10:03:22 +0200

    nog een upload?

--- Code/source/Drawing.lisp
+++ Code/source/Drawing.lisp
@@ -1,11 +1,44 @@
 (in-package :gdl-user)
 
+(defparameter *outputFolder* (merge-pathnames "../output/"
+					     (make-pathname :name nil
+							    :type nil
+							    :defaults excl:*source-pathname*))
+)
+
 (define-object Aircraft-tridrawing (base-drawing)
 
-:hidden-objects ((Aircraft :type 'Aircraft))
+:input-slots
+((outputFolder *outputFolder*))
+
+:hidden-objects ((Aircraft :type 'Aircraft)
+
+				 (text-block :type 'Aircraft-text-block
+							 :width (the text-view width)
+							 :length (the text-view length)
+							 :Fuselage-length nil
+							 :Fuselage-slenderness-ratio nil
+							 :Wing-Span nil
+							 :Wing-Sweep nil
+							 :Wing-Taper nil
+							 :Wing-Surface-Area nil
+							 :Engine-number nil
+							 :EnginePos nil
+							 ))
 
 :objects
-((tri-view :type 'base-view
+((text-view :type 'base-view
+			:left-margin
+			:right-margin
+			:border-box? t
+			:objects (list (the text-block))
+			:length (half (the length))
+			:width (the width)
+			:projection-vector (getf *standard-views* :top)
+			:center (translate (the center) 
+								:rear (half (the-child length))))
+
+(tri-view :type 'base-view
 			:border-box? t
 			:object-roots (list (the Aircraft fuselage) 
 								(the Aircraft engines)
@@ -13,10 +46,13 @@
 								(the Aircraft tail))
 			:length (the length)
 			:width (the width)
-			:center (the center)
+			:center (translate (the center)
+								:front (half (the-child length)))
 			:projection-vector (getf *standard-views* :trimetric)
 			)
 
+
+ ()			
 )
 )
 
@@ -64,10 +100,56 @@
 			)
 )
 
-;:functions
-;((write-drawing
-;	()
-;	(with-format (pdf (merge-pathnames "Aircraft-drawing.pdf" (*data-folder*))
-;						:page-length (the page-length) :page-width (the page-width)
-;	(write-the cad-output)))))
-)
+:functions
+((write-drawing
+	()
+	(with-format (pdf (merge-pathnames "Aircraft-drawing.pdf" (the outputFolder))
+						:page-length (the page-length) :page-width (the page-width)
+	(write-the cad-output))))
+	
+ (write-step
+	()
+	(with-format (step (merge-pathnames "Aircraft3D.stp" (the outputFolder))
+	(write-the cad-output-tree))))
+))
+
+(define-object Aircraft-text-block (typeset-block)
+
+:input-slots
+(Fuselage-length Fuselage-slenderness-ratio Wing-Span Wing-Sweep Wing-Taper Wing-Surface-Area Engine-number EnginePos)
+
+:functions
+((content
+ ()
+ (tt:compile-text(:font "Calibri" :font-size 12.0)
+ (tt:vspace 100)
+ (tt:paragraph () "Aircraft Data")
+ (tt:table (:col-widths (list 220 (- (the width) 220)))
+ (dolist (slot (list :Fuselage-length 
+					 :Fuselage-slenderness-ratio
+					 :Wing-Span
+					 :Wing-Sweep
+					 :Wing-Taper
+					 :Wing-Surface-Area
+					 :Engine-number
+					 :EnginePos))
+		(tt:row ()
+		  (tt:cell (:background-color "#1E1E1E") (tt:put-string (format nil "~a" (string-capitalize slot))))
+		  (tt:cell ()
+		  (tt:paragraph (:h-align :center) (tt:put-string (format nil "~a" (the (evaluate slot)))))))))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
