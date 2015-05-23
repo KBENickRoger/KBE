4818f6e3e034a840a2385f73ab86a816fc11e5a9
rjcoenen <rjcoenen@hotmail.com>
2015-05-23 16:54:42 +0200

    what i got sofar
    Vanavond testen, nosedroop en tailrotation angle inbouwen

--- Code/scratchpad/Fuselage.lisp
+++ Code/scratchpad/Fuselage.lisp
@@ -1,43 +1,68 @@
 (in-package :gdl-user)
 
-(define-object fuselagetail (base-object)
-:input-slots
-((diameter 2 :settable)
-(divergence 15 :settable)
-(lengthTail 15 :settable)
-(cross-section-percentages (list 0 0.5 0.9))
-(tailPoint (make-point 0 0 0))
-(lengthCenter 25)
-(lengthNose 5)
-)
-:computed-slots
-((section-offset-length  (mapcar #'(lambda (x) (* x (the lengthTail))) (the cross-section-percentages)))
+(defparameter *dataFolder* (merge-pathnames "../data/"
+					     (make-pathname :name nil
+							    :type nil
+							    :defaults excl:*source-pathname*))
+) 
 
-(section-radii (mapcar #'(lambda (z) (* (half (the diameter)) (- 1 z))) (the cross-section-percentages)))
+(define-object fuselage (cylinder-fuselage)
+ :input-slots
+ ((data-folder *data-folder*)
+   (fuselage-data-file (merge-pathnames "fuselage.dat" (the data-folder))))
+:d (6)
+:l (50)
+:cross-section-percents (the data fuselage-cross-section-percents)
+:objects
+  ((data :type 'aircraft-data
+:hidden? t
+:parameters (with-open-file (in (the fuselage-data-file)) (read in))
+:display-controls (list :color :red)
+)))
 
-(section-offset-divergence (mapcar #'(lambda (y1) (* (sin(the divergenceRAD)) y1)) (the section-offset-length)))
 
-(divergenceRAD (DEGREES-TO-RADIANS (the divergence)))
-)
+(define-object aircraft-data ()
+  :input-slots (
+(fuselage-cross-section-percents)
+))
 
+(define-object cylinder-fuselage (cylinder)
+  :input-slots (d l cross-section-percents)
 
+  :computed-slots ((radius (half (the d)))
+(length (the l))
 
-:objects 
-((merged :type 'merged-solid
+(section-offset-percentages (plist-keys (the cross-section-percents)))
+(section-centers (let ((nose-point (translate (the center) :front (half (the length)))))
+(mapcar #'(lambda (percentage)
+(translate nose-point :rear (* 1/100 percentage (the length))
+					  :down (if ((< (the-child index) 1) (half (half (the diameter)))) 
+								((> (the-child index) 7) (- 1))
+								(0))))
+					  
+				
+(the section-offset-percentages))))
+(section-diameter-percentages (plist-values (the cross-section-percents)))
+(section-radii (mapcar #'(lambda (percentage)
+(* 1/100 percentage (the radius)))
+(the section-diameter-percentages))))
+  
+  :objects ((regioned :type 'regioned-solid
+:display-controls (list :transparency 0.5)
+:brep (the merged))
+
+  :hidden-objects ((section-curves :type 'arc-curve
+:sequence (:size (length (the section-centers)))
+:center (nth (the-child index) (the section-centers))
+:radius (nth (the-child index) (the section-radii))
+:orientation (alignment :top (the (face-normal-vector :front))))
+
+(merged :type 'merged-solid
 :brep (the loft brep)
 :other-brep ()
 :make-manifold? t)
 
-(section-curves :type 'arc-curve
-:sequence (:size (length (the section-offset-length)))
-:center (translate (the tailPoint)
-					:rear (+ (the lengthNose) (the lengthCenter) (nth (the-child index) (the section-offset-length)))
-					:down (- (first (the section-radii)) (nth (the-child index) (the section-radii)))
-					:up (nth (the-child index) (the section-offset-divergence)))
-:radius (nth (the-child index) (the section-radii))
-:orientation (alignment :top (the (face-normal-vector :front))))
-
 (loft :type 'lofted-surface
 :end-caps-on-brep? t
-:curves (list-elements (the section-curves))))
+:curves (list-elements (the section-curves)))))
 )
