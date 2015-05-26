e22783d82f33aaacf7878f323d203446fad9fa7c
Madroon <nmnoordam@gmail.com>
2015-05-26 15:58:37 +0200

    moved aircraft-text-block to its own lisp file

--- Code/source/Drawing_main.lisp
+++ Code/source/Drawing_main.lisp
@@ -70,33 +70,3 @@
 		    :page-length (the page-length) :page-width (the page-width))
     (write-the cad-output)))))
 
-
-(define-object Aircraft-text-block (typeset-block)
-
-:input-slots
-((margins)
- (width)
- (length)
- (inputList))
-
-:computed-slots
-((keys (plist-keys (the inputList))))
-
-:functions
- (
- (content
-    ()
-    (tt:compile-text (:font "Helvetica" :font-size 12.0)
-      (tt:vspace 100)
-
-      (tt:paragraph () "Aircraft Data")
-      (let ((width (- (the width) (the margins))))
-	(tt:table (:col-widths (list (* 2/3 width) (* 1/3 width)))
-	  (dolist (slot (the keys))
-	    (tt:row ()
-	      (tt:cell (:background-color "#FFFFFF") (tt:put-string (format nil "~a" (string-capitalize slot))))
-	      (tt:cell () 
-		(tt:paragraph (:h-align :center) (tt:put-string (format nil "~a" (getf (the inputList) slot)))))))))))
-)
-
-)
